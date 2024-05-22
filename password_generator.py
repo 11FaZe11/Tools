9a93714c40a9f7a7795099210b76d7522d328b19
Abdo Zakaria <148268203+11FaZe11@users.noreply.github.com>
2024-05-22 23:26:26 +0300

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,15 @@
+import string
+import secrets
+
+def generate_password(length):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    while True:
+        password = ''.join(secrets.choice(alphabet) for i in range(length))
+        if (any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and any(c.isdigit() for c in password)
+                and any(c in string.punctuation for c in password)):
+            return password
+password_num = int(input("Enter the length of the password: "))
+
+print(generate_password(password_num))
